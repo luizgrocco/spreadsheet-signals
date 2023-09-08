d3f8c454435ab05de9d9ffaf4e6ed804dbf9d734
Luiz Rocco <luizgustavorocco@gmail.com>
2023-09-08 00:22:27 -0300

    Re-structure the Sheet visual component

--- src/components/DisplaySheet.tsx
+++ src/components/DisplaySheet.tsx
@@ -2,12 +2,60 @@ import "./DisplaySheet.css";
 import { colAsLabel, getCellIdFromRowCol } from "../utils";
 import { createMemo } from "../signals";
 import { Sheet } from "../models";
+import { FocusEvent, useCallback } from "react";
 
 const COLS = 10;
 const ROWS = 20;
 const grid = Array.from({ length: ROWS }, () => Array.from({ length: COLS }));
 
 export function DisplaySheet({ sheet }: { sheet: Sheet<number> }) {
+  const focusHandler = useCallback(
+    (rowIndex: number, colIndex: number) =>
+      (event: FocusEvent<HTMLInputElement>) => {
+        const cellId = getCellIdFromRowCol(rowIndex, colIndex);
+        const cell = sheet.get(cellId);
+        if (cell) {
+          event.target.value = cell.originalContent;
+        }
+        event.target.select();
+      },
+    [sheet]
+  );
+
+  const blurHandler =
+    (rowIndex: number, colIndex: number) =>
+    (event: FocusEvent<HTMLInputElement>) => {
+      const cellId = getCellIdFromRowCol(rowIndex, colIndex);
+      const cell = sheet.get(cellId);
+
+      const originalContents = event.target.value;
+      const updateFn = event.target.value.startsWith("=")
+        ? () => {
+            const value = sheet.evaluateFormula(originalContents);
+            event.target.value = String(value);
+            return value;
+          }
+        : () => {
+            const value = Number(originalContents);
+            event.target.value = String(value);
+            return value;
+          };
+
+      if (!cell) {
+        if (originalContents === "") return;
+        sheet.insert({
+          cellId,
+          originalContent: originalContents,
+          computed: createMemo(updateFn),
+        });
+      } else {
+        cell.originalContent = originalContents;
+        originalContents === ""
+          ? cell.computed.set(() => 0)
+          : cell.computed.set(updateFn);
+      }
+    };
+
   return (
     <div className="sheet">
       <div className="grid-row">
@@ -24,50 +72,13 @@ export function DisplaySheet({ sheet }: { sheet: Sheet<number> }) {
           {row.map((_, colIndex) => (
             <input
               type="text"
-              key={`${rowIndex}${colIndex}`}
+              key={`${colIndex}${rowIndex}`}
               className="grid-cell"
               onKeyDown={(event) => {
                 if (event.key === "Enter") event.currentTarget.blur();
               }}
-              onFocus={(event) => {
-                const cellId = getCellIdFromRowCol(rowIndex, colIndex);
-                const cell = sheet.get(cellId);
-                if (cell) {
-                  event.target.value = cell.originalContent;
-                }
-                event.target.select();
-              }}
-              onBlur={(event) => {
-                const cellId = getCellIdFromRowCol(rowIndex, colIndex);
-                const cell = sheet.get(cellId);
-
-                const originalContents = event.target.value;
-                const updateFn = event.target.value.startsWith("=")
-                  ? () => {
-                      const value = sheet.evaluateFormula(originalContents);
-                      event.target.value = String(value);
-                      return value;
-                    }
-                  : () => {
-                      const value = Number(originalContents);
-                      event.target.value = String(value);
-                      return value;
-                    };
-
-                if (!cell) {
-                  if (originalContents === "") return;
-                  sheet.insert({
-                    cellId,
-                    originalContent: originalContents,
-                    computed: createMemo(updateFn),
-                  });
-                } else {
-                  cell.originalContent = originalContents;
-                  originalContents === ""
-                    ? cell.computed.set(() => 0)
-                    : cell.computed.set(updateFn);
-                }
-              }}
+              onFocus={focusHandler(rowIndex, colIndex)}
+              onBlur={blurHandler(rowIndex, colIndex)}
             />
           ))}
         </div>
